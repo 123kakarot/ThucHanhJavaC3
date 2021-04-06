7c40133af5a31efb0009fc13bf1e96b803dbc14f
123kakarot <ten89849@gmail.com>
2021-04-06 20:51:33 +0700

    Up lai file Tuan3

--- LaB3/src/lab3/bai1.java
+++ LaB3/src/lab3/bai1.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab3;
 
 import javax.swing.JFrame;
@@ -13,12 +8,13 @@ import javax.swing.JLabel;
  * @author Administrator
  */
 public class bai1 {
-     public static void main(String[] args){
- JFrame myFrame = new JFrame("Hello World!");
- myFrame.setSize(300, 150); //kích thước JFrame
- myFrame.setVisible(true);
- JLabel lbName=new JLabel();
- lbName.setText("Chào các bạn!");
- myFrame.add(lbName);// Thêm jLabel vài JFrame
- }
+
+    public static void main(String[] args) {
+        JFrame myFrame = new JFrame("Hello World!");
+        myFrame.setSize(300, 150); //kích thước JFrame
+        myFrame.setVisible(true);
+        JLabel lbName = new JLabel();
+        lbName.setText("Chào các bạn!");
+        myFrame.add(lbName);// Thêm jLabel vài JFrame
+    }
 }
--- LaB3/src/lab3/bai5.java
+++ LaB3/src/lab3/bai5.java
@@ -1,43 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab3;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class bai5 extends JFrame{
-    
-    public  bai5(String title){
+public class bai5 extends JFrame {
+
+    public bai5(String title) {
         JPanel pnBorder = new JPanel();
-            pnBorder.setLayout(new BorderLayout());
+        pnBorder.setLayout(new BorderLayout());
         JPanel pnNorth = new JPanel();
-            pnNorth.setBackground(Color.RED);
-            pnBorder.add(pnNorth, BorderLayout.NORTH);
+        pnNorth.setBackground(Color.RED);
+        pnBorder.add(pnNorth, BorderLayout.NORTH);
         JPanel pnSouth = new JPanel();
-            pnSouth.setBackground(Color.RED);
-            pnBorder.add(pnSouth, BorderLayout.SOUTH);
+        pnSouth.setBackground(Color.RED);
+        pnBorder.add(pnSouth, BorderLayout.SOUTH);
         JPanel pnWest = new JPanel();
-            pnWest.setBackground(Color.BLUE);
-            pnBorder.add(pnWest, BorderLayout.WEST);
+        pnWest.setBackground(Color.BLUE);
+        pnBorder.add(pnWest, BorderLayout.WEST);
         JPanel pnEast = new JPanel();
-            pnEast.setBackground(Color.BLUE);
-            pnBorder.add(pnEast, BorderLayout.EAST);
+        pnEast.setBackground(Color.BLUE);
+        pnBorder.add(pnEast, BorderLayout.EAST);
         JPanel pnCenter = new JPanel();
-            pnCenter.setBackground(Color.YELLOW);
-            pnBorder.add(pnCenter, BorderLayout.CENTER);
+        pnCenter.setBackground(Color.YELLOW);
+        pnBorder.add(pnCenter, BorderLayout.CENTER);
         getContentPane().add(pnBorder);
     }
 
     public static void main(String[] args) {
         bai5 myUI = new bai5("Demo BorderLayout");
-        myUI.setSize(600,100);
+        myUI.setSize(600, 100);
         myUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myUI.setLocationRelativeTo(null);
         myUI.setVisible(true);
     }
-    
+
 }
